a3c9e466958d374768293523b1acfed84f0b561e
David <djhatvr@gmail.com>
2024-01-14 14:38:04 -0800

    Add Mouse Look

--- Daves Custom Packages/Assets/com.davidhopetech.tools/Editor/Tools.cs
+++ Daves Custom Packages/Assets/com.davidhopetech.tools/Editor/Tools.cs
@@ -109,6 +109,13 @@ namespace com.davidhopetech.tools.Run_Time.Editor
 		}
 
 
+		[MenuItem("GameObject/Davids Tools/Select All Canvases", false, 10)]
+		static void SelectAllCanvases(MenuCommand menuCommand)
+		{
+			SelectAllGameObjectTypes<Canvas>();
+		}
+
+
 		[MenuItem("GameObject/Davids Tools/Select All Box Colliders", false, 10)]
 		static void SelectAllBoxColliders(MenuCommand menuCommand)
 		{
