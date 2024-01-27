8186bf9cb53b2369f08be568b43143521d3704df
David <djhatvr@gmail.com>
2024-01-27 15:04:49 -0800

    Add  Recenter function which does not work on Quest, Update Service Scene

---
+++ Daves Custom Packages/Assets/com.davidhopetech.core/Run Time/Scripts/Utils/RecenterOrigin.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using com.davidhopetech.core.Run_Time.Scripts.Service_Locator;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class RecenterOrigin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Recenter();
+    }
+
+    
+    
+    // #########################################################################################################
+    // ###########                                                                                   ###########
+    // ###########                 Recentering Programatically is not Supported by Meta             ############
+    // ###########                                                                                   ###########
+    // #########################################################################################################
+    
+    public void Recenter()
+    {
+        DHTServiceLocator.Get<DHTLogService>().Log("------  Attempting to recenter  ------\n");
+        List<XRInputSubsystem> subsystems = new List<XRInputSubsystem>();
+        SubsystemManager.GetInstances<XRInputSubsystem>(subsystems);
+
+        if (subsystems.Count > 0)
+        {
+            XRInputSubsystem inputSubsystem = subsystems[0];
+            if (inputSubsystem != null)
+            {
+                var succesful = inputSubsystem.TryRecenter();
+                var succesStr = succesful ? "Succesfull" : "Not succesful";
+                DHTServiceLocator.Get<DHTLogService>().Log($"{succesStr}\n");
+            }
+        }
+    }
+}
