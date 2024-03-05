b8f6375485b384bba07f23366b9e7fd5cec0d186
David <djhatvr@gmail.com>
2024-03-04 17:22:44 -0800

    Move Netcode Dependency to Package Fix MicrophonePermission request

--- Daves Custom Packages/Assets/com.davidhopetech.core/Run Time/Scripts/Service/MicrophonePermission.cs
+++ Daves Custom Packages/Assets/com.davidhopetech.core/Run Time/Scripts/Service/MicrophonePermission.cs
@@ -3,7 +3,7 @@ using UnityEngine.Android;
 
 public class MicrophonePermission : MonoBehaviour
 {
-	void Start()
+	public void CheckMicrophonePermission()
 	{
 		// Check if the user has already granted the RECORD_AUDIO permission
 		if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
