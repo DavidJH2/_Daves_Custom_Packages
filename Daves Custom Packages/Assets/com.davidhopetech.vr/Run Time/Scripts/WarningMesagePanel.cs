7000d2b3d9103811cfa31bff2ccc124bcd58415d
David <djhatvr@gmail.com>
2024-03-08 20:30:11 -0800

    Fix Spelling Error

--- Daves Custom Packages/Assets/com.davidhopetech.vr/Run Time/Scripts/WarningMesagePanel.cs
+++ Daves Custom Packages/Assets/com.davidhopetech.vr/Run Time/Scripts/WarningMesagePanel.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace com.davidhopetech.vr.Run_Time.Scripts
 {
-    public class WarningMesagePannel : MonoBehaviour
+    public class WarningMesagePanel : MonoBehaviour
     {
         // Start is called before the first frame update
         void Start()
