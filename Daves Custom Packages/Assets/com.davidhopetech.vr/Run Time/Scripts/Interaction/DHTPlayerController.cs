0853083d5a4f09dea94c52a9310aa41053ab4722
David <djhatvr@gmail.com>
2024-06-14 08:02:13 -0700

    Remove Old Input System

--- Daves Custom Packages/Assets/com.davidhopetech.vr/Run Time/Scripts/Interaction/DHTPlayerController.cs
+++ Daves Custom Packages/Assets/com.davidhopetech.vr/Run Time/Scripts/Interaction/DHTPlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using com.davidhopetech.core.Run_Time.Extensions;
@@ -5,6 +6,7 @@ using com.davidhopetech.vr.Run_Time.Scripts.Interaction.States;
 using TMPro;
 using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -13,14 +15,15 @@ namespace com.davidhopetech.vr.Run_Time.Scripts.Interaction
 	public class DHTPlayerController : MonoBehaviour
 	{
 		//[SerializeField] internal InputDeviceCharacteristics controllerCharacteristics;
-		[SerializeField] internal DHTInteractionState leftHandInitialInteractionState;
-		[SerializeField] internal DHTInteractionState rightHandInitialInteractionState;
-		[SerializeField] internal GameObject          leftMirrorHand;
-		[SerializeField] internal GameObject          rightMirrorHand;
-		[SerializeField] internal float               handSpringCoeeff; // TODO: Move these to Settings Scriptable Object
-		[SerializeField] internal float               handDampCoeeff;
-		[SerializeField] private  DHTJoystick         dhtJoystick;
-		[SerializeField] internal float               throwMultiplyer = 2.0f;
+		[SerializeField] internal DHTInteractionState  leftHandInitialInteractionState;
+		[SerializeField] internal DHTInteractionState  rightHandInitialInteractionState;
+		[SerializeField] internal GameObject           leftMirrorHand;
+		[SerializeField] internal GameObject           rightMirrorHand;
+		[SerializeField] internal float                handSpringCoeeff; // TODO: Move these to Settings Scriptable Object
+		[SerializeField] internal float                handDampCoeeff;
+		[SerializeField] private  DHTJoystick          dhtJoystick;
+		[SerializeField] internal float                throwMultiplyer = 2.0f;
+		[SerializeField] private  InputActionReference _quitActon;
 
 		// private                   InputDevice                targetDevice;
 
@@ -30,6 +33,21 @@ namespace com.davidhopetech.vr.Run_Time.Scripts.Interaction
 		internal DHTInteractionStateRef RightHandInteractionStateRef;
 
 		private XROrigin _xrOrgin;
+
+
+		private void OnEnable()
+		{
+			if (_quitActon)
+			{
+				_quitActon.action.Enable();
+				_quitActon.action.performed += Quit;
+			}
+			else
+			{
+				Debug.LogWarning("quit action reference is not set.");
+			}
+		}
+
 
 		private void Awake()
 		{
@@ -61,11 +79,11 @@ namespace com.davidhopetech.vr.Run_Time.Scripts.Interaction
 
 		public void OnActivate(ActivateEventArgs args)
 		{
-			
+
 		}
 
 
-	public void OnSelectEnter(SelectEnterEventArgs args)
+		public void OnSelectEnter(SelectEnterEventArgs args)
 		{
 			var a = args.interactableObject;
 		}
@@ -73,7 +91,7 @@ namespace com.davidhopetech.vr.Run_Time.Scripts.Interaction
 
 		public void OnfocusExited(FocusExitEventArgs args)
 		{
-			
+
 		}
 
 		public void SetVRMode(TMP_Dropdown dropdown)
@@ -89,16 +107,26 @@ namespace com.davidhopetech.vr.Run_Time.Scripts.Interaction
 			}
 		}
 
-	// UpdateStateImpl is called once per frame
+		// UpdateStateImpl is called once per frame
 		void Update()
 		{
 			RightHandInteractionStateRef.InteractionState.UpdateState();
 			LeftHandInteractionStateRef.InteractionState.UpdateState();
+		}
 
-			if (Input.GetKeyDown(KeyCode.Escape))
+
+		void Quit(InputAction.CallbackContext context)
+		{
+			if (context.performed)
 			{
 				Application.Quit();
 			}
+		}
+
+
+		private void OnDisable()
+		{
+			_quitActon.action.Disable();
 		}
 	}
 }
